48876886a3987d7ae3a23216c1f7e905571a7e15
alexbertschi <alex.bertschi@gmail.com>
2021-01-16 11:14:41 +0100

    first hello world commit

---
+++ src/test/java/helloworldalex/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package helloworldalex;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println(" Alexes hello world");
+	}
+
+}
